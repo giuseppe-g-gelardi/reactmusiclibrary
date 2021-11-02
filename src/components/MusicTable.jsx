6881b35934b570bbb5c8aba12c3e4bd248030fb5
n-rdy <giuseppegelardi@icloud.com>
2021-11-02 00:27:30 -0500

    better implementation of the table

--- src/components/MusicTable.jsx
+++ src/components/MusicTable.jsx
@@ -3,36 +3,32 @@ import { Table } from 'react-bootstrap'
 
 export default function MusicTable({ songList }) {
 
-  console.log(songList)
-  // song title, album, artist, genre, and release date
- 
-
   return (
     <>
-      {songList.map((songs, index) => {
-        return (
-          <Table striped bordered hover>
-            <thead>
-              <tr>
-                <th>Song Title</th>
-                <th>Album</th>
-                <th>Artist</th>
-                <th> Genre</th>
-                <th>Release Date</th>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Song Title</th>
+            <th>Album</th>
+            <th>Artist</th>
+            <th> Genre</th>
+            <th>Release Date</th>
+          </tr>
+        </thead>
+        <tbody>
+            {songList.map((song, key) => {
+            return (
+              <tr key={key}>
+                <td>{song.title}</td>
+                <td>{song.album}</td>
+                <td>{song.artist}</td>
+                <td>{song.genre}</td>
+                <td>{song.releaseDate}</td>
               </tr>
-            </thead>
-            <tbody>
-              <tr>
-                  <td>{songs.title}</td>
-                  <td>{songs.album}</td>
-                  <td>{songs.artist}</td>
-                  <td>{songs.genre}</td>
-                  <td>{songs.releaseDate}</td>
-              </tr>
-            </tbody>
-          </Table>
-        )
-      })}
+            )
+          })}
+        </tbody>
+      </Table>
     </>
   )
 }
