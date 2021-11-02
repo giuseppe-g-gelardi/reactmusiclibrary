ff388711fd79b0f547cdb9c62b127f9ae6a1fedd
n-rdy <giuseppegelardi@icloud.com>
2021-11-01 22:58:52 -0500

    add get request, endpoint getting list of songs

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 export default function App() {
+
+  const api = 'http://www.devcodecampmusiclibrary.com/'
+  // song title, album, artist, genre, and release date
+
+  useEffect(() => {
+    getSongs()
+  })
+
+  const getSongs = async () => {
+    try {
+      axios.get(`${api}/api/music`).then(
+        response => {
+          console.log(response.data)
+        },
+        err => {
+          console.log(err)
+        }
+      )
+    } catch (except) {
+      console.log(except)
+    }
+  }
+
+
   return (
     <>
       <h1>hello world</h1>
