7db9a49b7d6988ac7a78da9d611093f36bde7576
n-rdy <giuseppegelardi@icloud.com>
2021-11-02 00:15:57 -0500

    implement mapped table

--- src/components/MusicTable.jsx
+++ src/components/MusicTable.jsx
@@ -1,10 +1,40 @@
 import React from 'react'
+import { Table } from 'react-bootstrap'
 
 export default function MusicTable({ songList }) {
 
   console.log(songList)
+  // song title, album, artist, genre, and release date
+ 
 
   return (
-    <h1>music table</h1>
+    <>
+      {songList.map((songs, index) => {
+        return (
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>Song Title</th>
+                <th>Album</th>
+                <th>Artist</th>
+                <th> Genre</th>
+                <th>Release Date</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                  <td>{songs.title}</td>
+                  <td>{songs.album}</td>
+                  <td>{songs.artist}</td>
+                  <td>{songs.genre}</td>
+                  <td>{songs.releaseDate}</td>
+              </tr>
+            </tbody>
+          </Table>
+        )
+      })}
+    </>
   )
 }
+
+
