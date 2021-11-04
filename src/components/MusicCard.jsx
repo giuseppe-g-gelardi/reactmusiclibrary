eb37ecf42d0b6e5d7103f7ced5bb7fa616284a7d
n-rdy <giuseppegelardi@icloud.com>
2021-11-04 15:16:08 -0500

    map song data to mui cards instead of table, will add filter

---
+++ src/components/MusicCard.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import Card from '@material-ui/core/Card'
+import CardHeader from '@material-ui/core/CardHeader'
+import CardContent from '@material-ui/core/CardContent'
+import { Avatar, IconButton, makeStyles, Typography } from '@material-ui/core'
+import { DeleteOutline } from '@material-ui/icons'
+import { blue, green, pink, yellow } from '@material-ui/core/colors'
+
+const useStyles = makeStyles({
+  avatar: {
+    backgroundColor: (note) => {
+      if (note.category === 'work') {
+        return yellow[700]
+      }
+      if (note.category === 'money') {
+        return green[500]
+      }
+      if (note.category === 'todos') {
+        return pink[700]
+      }
+      return blue[500]
+    }
+  }
+})
+
+export default function MusicCard({ song, handleDelete }) {
+
+  const classes = useStyles(song)
+
+  return (
+    <div>
+      <Card elevation={3} className={classes.test}>
+        <CardHeader 
+          avatar={
+            <Avatar className={classes.avatar}>
+              {song.title[0].toUpperCase()}
+            </Avatar>
+          }
+          action={
+            <IconButton onClick={() => handleDelete(song.id)}>
+              <DeleteOutline />
+            </IconButton>
+          } 
+          title={song.title}
+          subheader={song.artist}     
+        />
+        <CardContent>
+          <Typography variant='body2' color='textSecondary'>
+            Album: {song.album} <br />
+            Release Date: {song.releaseDate}
+          </Typography>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
--- src/components/MusicTable.jsx
+++ src/components/MusicTable.jsx
@@ -5,6 +5,9 @@ import TextField from '@mui/material/TextField'
 import { makeStyles } from '@material-ui/core/styles'
 import Button from '@material-ui/core/Button'
 import DeleteIcon from '@material-ui/icons/Delete';
+import Grid from '@material-ui/core/Grid'
+import { Container } from '@material-ui/core'
+import MusicCard from './MusicCard'
 
 const useStyles = makeStyles({
   field: {
@@ -57,77 +60,16 @@ export default function MusicTable() {
   }
 
   return (
-    <div>
-  
-      <div id='table-filter' className='song-table'>
 
-        <TextField 
-        label='Filter songs...'
-        className={classes.field}
-          onChange={e => {
-            setSearchTerm(e.target.value)
-          }}
-        />
-
-        <br />
-        <div className='table'>
-          <Table responsive striped bordered hover variant='light'>
-            <thead>
-              <tr>
-                <th>Song Title</th>
-                <th>Album</th>
-                <th>Artist</th>
-                <th> Genre</th>
-                <th>Release Date</th>
-              </tr>
-            </thead>
-            <tbody>
-              {songList
-                // eslint-disable-next-line array-callback-return
-                .filter(val => {
-                  let searchString = ''
-                  for (let value of Object.entries(val)) {
-                    searchString += `${value}\t`
-                  }
-                  if (searchTerm === '') {
-                    return val
-                  } else if (
-                    searchString
-                      .toLowerCase()
-                      .includes(searchTerm.toLowerCase())) {
-                    return val
-                  }
-                })
-                .map((val, key) => {
-                  return (
-                    <tr key={key}>
-                      <td>{val.title}</td>
-                      <td>{val.album}</td>
-                      <td>{val.artist}</td>
-                      <td>{val.genre}</td>
-                      <td>{val.releaseDate}</td>
-                      <td>
-                   
-                        <Button
-                          className={classes.btn}
-                          onClick={() => handleDelete(val.id)}
-                          type='submit'
-                          color='secondary'
-                          variant='contained'
-                          endIcon={<DeleteIcon />}
-                        >
-                          Delete
-                        </Button> 
-                      
-                      </td>
-                    </tr>
-                  )
-                })}
-            </tbody>
-          </Table>
-        </div>
-      </div>
-    </div>
+      <Container>
+        <Grid container spacing={3}>
+          {songList.map(song => (
+            <Grid item key={song.id} sx={12} md={6} lg={4}> 
+              <MusicCard song={song} handleDelete={handleDelete} />
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
   )
 }
 
